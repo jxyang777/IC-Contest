c055da7cac36d842244f73793e487a519e9ec3fd
jxyang <44730359+jxyang777@users.noreply.github.com>
2023-04-21 03:26:12 +0800

    Add files via upload

--- Preliminary/2023/LASER.v
+++ Preliminary/2023/LASER.v
@@ -10,7 +10,6 @@ output reg [3:0] C2Y,
 output reg DONE);
 
     // State
-    parameter IDLE           = 3'd0;
     parameter RECEIVE        = 3'd1;
     parameter FIND_MAX_COVER = 3'd2;
     parameter CHECK_1        = 3'd3;
@@ -22,24 +21,23 @@ output reg DONE);
     reg [2:0] next_state;
 
     reg [5:0] receive_count;
-    reg [5:0] target_pos;       // dot for calculating distance 
+    reg [5:0] target_idx;                                      // dot for calculating distance 
 
-    reg [5:0] tem_cover_count; // cover count of every scan of 40 targets
-    reg [5:0] max_cover_count; // max cover count of every scan of 256 dots
-    reg [5:0] iter_cover_count; // max cover count of all iteration
+    reg [5:0] tem_cover_count;                                 // cover count of every scan of 40 targets
+    reg [5:0] max_cover_count;                                 // max cover count of every scan of 256 dots
+    reg [5:0] iter_cover_count;                                // max cover count of all iteration
 
-    reg [3:0] target_pos_x, target_pos_y; // x, y of target_pos for calculating distance
-    reg [3:0] d_x1, d_y1;                 // abs of intercept of target_pos and search_pos
-    reg [3:0] d_x2, d_y2;                 // abs of intercept of fix_pos and search_pos
-    reg [8:0] dist_1, dist_2;             // distance square
+    wire [3:0] x_target, y_target;                     // x, y of target_idx for calculating distance
+    wire [3:0] dist_x1, dist_y1;                               // abs of intercept of target_idx and search_pos
+    wire [3:0] dist_x2, dist_y2;                               // abs of intercept of fix_pos and search_pos
+    wire [8:0] dist_sq1, dist_sq2;                             // distance square
 
-    reg [3:0] x, y;             // position for searching
-    reg [3:0] x_l_limit, x_r_limit, y_u_limit, y_d_limit; // limit of position
-    reg [3:0] x_fix, y_fix;     // fixed position
-    reg [3:0] x_max, y_max;     // position with fixed position have max cover count
-    reg [3:0] x1_iter, y1_iter; // position 1
-    reg [3:0] x2_iter, y2_iter; // position 2
-                                // position 1 and postion 2 are the two positions which can cover most targets simultaneously after one iteration
+    reg [3:0] x, y;                                            // position for searching
+    reg [3:0] x_l_limit, x_r_limit, y_u_limit, y_d_limit;      // limit of position
+    reg [3:0] x_fix, y_fix;                                    // fixed position
+    reg [3:0] x_max, y_max;                                    // position with fixed position have max cover count
+    reg [3:0] x1_iter, y1_iter;                                // position 1
+    reg [3:0] x2_iter, y2_iter;                                // position 2               // position 1 and postion 2 are the two positions which can cover most targets simultaneously after one iteration
     
     reg [3:0] Xlist [0:39];
     reg [3:0] Ylist [0:39];
@@ -47,47 +45,46 @@ output reg DONE);
 
     
     // State update
-    always @(posedge CLK or RST) begin
-        if(RST)
-            current_state <= IDLE;
-        else
-            current_state <= next_state;
+    always @(posedge CLK) begin
+        if(RST) current_state <= RECEIVE;
+        else    current_state <= next_state;
     end
 
     // Next state logic
     always @(*) begin
         case (current_state)
-            IDLE: next_state <= RECEIVE;
             RECEIVE: begin
-                if(receive_count >= 6'd40)
-                    next_state = FIND_MAX_COVER;
-                else
-                    next_state = RECEIVE;
+                if(receive_count >= 6'd40) next_state = FIND_MAX_COVER;
+                else                       next_state = RECEIVE;
             end
             FIND_MAX_COVER: begin
-                if(target_pos >= 6'd39)
-                    next_state = CHECK_1;
-                else
-                    next_state = FIND_MAX_COVER;
+                if(target_idx >= 6'd39) next_state = CHECK_1;
+                else                    next_state = FIND_MAX_COVER;
             end
             CHECK_1: begin
-                if(x == x_r_limit && y == y_d_limit)
-                    next_state = CHECK_2;
-                else
-                    next_state = FIND_MAX_COVER; 
+                if(x == x_r_limit && y == y_d_limit) next_state = CHECK_2;
+                else                                 next_state = FIND_MAX_COVER; 
             end
             CHECK_2: begin
-                if(max_cover_count == iter_cover_count && x1_iter == x_max && y1_iter == y_max && x2_iter == x_fix && y2_iter == y_fix)
+                if(max_cover_count == iter_cover_count 
+                    && x1_iter == x_max && y1_iter == y_max 
+                    && x2_iter == x_fix && y2_iter == y_fix)
                     next_state = FINISH;
                 else
                     next_state = FIX_POINT;
             end
-            FIX_POINT: begin
-                next_state = FIND_MAX_COVER;
-            end
+            FIX_POINT: next_state = FIND_MAX_COVER;
             FINISH: next_state = RECEIVE;
-            default: next_state = IDLE;
         endcase
+    end
+
+    // receive_count
+    always @(posedge CLK) begin
+        if(RST || DONE)
+            receive_count <= 6'd0;
+        else if(current_state == RECEIVE) begin
+            receive_count <= receive_count + 1'd1;
+        end
     end
 
     // Xlist, Ylist
@@ -98,24 +95,60 @@ output reg DONE);
         end
     end
 
-    // iteration
+    // target_idx
     always @(posedge CLK) begin
-        if(RST || DONE)
-            iteration <= 4'd0;
-        else if(current_state == FIX_POINT)
-            iteration <= iteration + 1'd1; 
+        if(RST || DONE || target_idx == 6'd39)
+            target_idx <= 6'd0;
+        else if(current_state == FIND_MAX_COVER)
+            target_idx <= target_idx + 1'd1;
+    end
+
+    // x, y
+    always @(posedge CLK) begin
+        if(RST || DONE) begin
+            x <= 4'd0;
+            y <= 4'd0;
+        end
+        else if(current_state == FIX_POINT) begin
+            x <= x_l_limit;
+            y <= y_u_limit;
+        end
+        else if(current_state == CHECK_1) begin
+            if(x == x_r_limit) begin
+                x <= x_l_limit;
+                y <= y + 1'd1;
+            end
+            else
+                x <= x + 1'd1;
+        end
+    end
+
+    // x_l_limit, x_r_limit, y_u_limit, y_d_limit
+    always @(posedge CLK) begin
+        if(RST || DONE) begin
+            x_l_limit <= 4'd0;
+            x_r_limit <= 4'd15;
+            y_u_limit <= 4'd0;
+            y_d_limit <= 4'd15;
+        end
+        else if(next_state == FIX_POINT) begin
+            x_l_limit <= (x_max > 4'd7) ? (x_max - 4'd7) : 4'd0;
+            x_r_limit <= (x_max < 4'd8) ? (x_max + 4'd7) : 4'd15;
+            y_u_limit <= (y_max > 4'd7) ? (y_max - 4'd7) : 4'd0;
+            y_d_limit <= (y_max < 4'd8) ? (y_max + 4'd7) : 4'd15;
+        end
     end
 
     // distance square
-    assign target_pos_x = Xlist[target_pos];
-    assign target_pos_y = Ylist[target_pos];
-    assign d_x1 = (target_pos_x >= x) ? (target_pos_x-x) : (x-target_pos_x);  // abs
-    assign d_y1 = (target_pos_y >= y) ? (target_pos_y-y) : (y-target_pos_y);  // abs
-    assign dist_1 = d_x1**2 + d_y1**2;
+    assign x_target = Xlist[target_idx];
+    assign y_target = Ylist[target_idx];
+    assign dist_x1 = (x_target >= x) ? (x_target-x) : (x-x_target);  // abs
+    assign dist_y1 = (y_target >= y) ? (y_target-y) : (y-y_target);  // abs
+    assign dist_sq1 = dist_x1**2 + dist_y1**2;
 
-    assign d_x2 = (target_pos_x >= x_fix) ? (target_pos_x-x_fix) : (x_fix-target_pos_x);
-    assign d_y2 = (target_pos_y >= y_fix) ? (target_pos_y-y_fix) : (y_fix-target_pos_y);
-    assign dist_2 = d_x2**2 + d_y2**2;
+    assign dist_x2 = (x_target >= x_fix) ? (x_target-x_fix) : (x_fix-x_target);
+    assign dist_y2 = (y_target >= y_fix) ? (y_target-y_fix) : (y_fix-y_target);
+    assign dist_sq2 = dist_x2**2 + dist_y2**2;
 
     // tem_cover_count
     always @(posedge CLK) begin
@@ -123,9 +156,9 @@ output reg DONE);
             tem_cover_count <= 6'd0;
         else if(current_state == FIND_MAX_COVER) begin
             if(iteration == 0)
-                tem_cover_count <= tem_cover_count + (dist_1 <= 9'd16);
+                tem_cover_count <= tem_cover_count + (dist_sq1 <= 9'd16);
             else
-                tem_cover_count <= tem_cover_count + ((dist_1 <= 9'd16) || (dist_2 <= 9'd16));
+                tem_cover_count <= tem_cover_count + ((dist_sq1 <= 9'd16) || (dist_sq2 <= 9'd16));
         end
     end
 
@@ -149,59 +182,12 @@ output reg DONE);
         end
     end
 
-    // x
+    // iteration
     always @(posedge CLK) begin
         if(RST || DONE)
-            x <= 4'd0;
+            iteration <= 4'd0;
         else if(current_state == FIX_POINT)
-            x <= x_l_limit;
-        else if(current_state == CHECK_1) begin
-            if(x == x_r_limit)
-                x <= x_l_limit;
-            else
-                x <= x + 1'd1;
-        end
-    end
-
-    // y
-    always @(posedge CLK) begin
-        if(RST || DONE)
-            y <= 4'd0;
-        else if(current_state == FIX_POINT)
-            y <= y_u_limit;
-        else if(current_state == CHECK_1) begin
-            if(x == x_r_limit)
-                y <= y + 1'd1;
-        end
-    end
-
-    // x_l_limit, x_r_limit, y_l_limit, y_r_limit
-    always @(posedge CLK) begin
-        if(RST || DONE) begin
-            x_l_limit <= 4'd0;
-            x_r_limit <= 4'd15;
-            y_u_limit <= 4'd0;
-            y_d_limit <= 4'd15;
-        end
-        else if(next_state == FIX_POINT) begin
-            x_l_limit <= (x_max > 4'd7) ? (x_max - 4'd7) : 4'd0;
-            x_r_limit <= (x_max < 4'd8) ? (x_max + 4'd7) : 4'd15;
-            y_u_limit <= (y_max > 4'd7) ? (y_max - 4'd7) : 4'd0;
-            y_d_limit <= (y_max < 4'd8) ? (y_max + 4'd7) : 4'd15;
-        end
-    end
-
-
-    // x_fix, y_fix
-    always @(posedge CLK) begin
-        if(RST || DONE) begin
-            x_fix <= 4'd0;
-            y_fix <= 4'd0;
-        end
-        else if(next_state == FIX_POINT) begin
-            x_fix <= x_max;
-            y_fix <= y_max;
-        end
+            iteration <= iteration + 1'd1; 
     end
 
     // x_max, y_max
@@ -215,6 +201,18 @@ output reg DONE);
                 x_max <= x;
                 y_max <= y;
             end
+        end
+    end
+
+    // x_fix, y_fix
+    always @(posedge CLK) begin
+        if(RST || DONE) begin
+            x_fix <= 4'd0;
+            y_fix <= 4'd0;
+        end
+        else if(next_state == FIX_POINT) begin
+            x_fix <= x_max;
+            y_fix <= y_max;
         end
     end
 
@@ -236,31 +234,13 @@ output reg DONE);
         end
     end
 
-    // receive_count
-    always @(posedge CLK) begin
-        if(RST || DONE)
-            receive_count <= 6'd0;
-        else if(current_state == RECEIVE) begin
-            receive_count <= receive_count + 1'd1;
-        end
-    end
-
-    // target_pos
-    always @(posedge CLK) begin
-        if(RST || DONE || target_pos == 6'd39)
-            target_pos <= 6'd0;
-        else if({current_state == FIND_MAX_COVER})
-            target_pos <= target_pos + 1'd1;
-    end
- 
-    // OUTPUT
-    
+    // OUTPUT 
     // C1X, C1Y, C2X, C2Y
-    always @(posedge CLK) begin
-        C1X <= x1_iter;
-        C1Y <= y1_iter;
-        C2X <= x2_iter;
-        C2Y <= y2_iter;
+    always @(*) begin
+        C1X = x1_iter;
+        C1Y = y1_iter;
+        C2X = x2_iter;
+        C2Y = y2_iter;
     end
 
     // DONE
